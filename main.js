f8f48c1a62fd9999835eadb2b483e28701269516
StuffThing132 <76539609+StuffThing132@users.noreply.github.com>
2022-01-07 11:34:22 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function tv(){
+    window.location = "tv.html";
+}
+function desk(){
+    window.location = "desk.html";
+}
+function bottle(){
+    window.location = "bottle.html";
+}
+function fruitbasket(){
+    window.location = "fruitbasket.html";
+}
+
